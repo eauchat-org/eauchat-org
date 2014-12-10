25b0ff3f374bad0c8b2fe4b1c933a3d99241719e
Sébastien Lerique <sl@mehho.net>
2014-12-10 23:14:18 +0100

    Add footer image

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 "use strict";
 
 $(document).ready(function() {
+  /*
+   * Set handlers for hovering on menu items
+   */
   var appsContainer = $('div#apps'),
       apps = appsContainer.find('ul.apps').children(),
       descriptionsContainer = $('#descriptions'),
@@ -26,4 +29,27 @@ $(document).ready(function() {
     // Attach handlers
     appAnchor.hover(hoverIn, hoverOut);
   }
+
+  /*
+   * Load SVG images
+   */
+  $('#footer').load('/images/home-footer.svg', function() {
+    var footer = $('#footer svg'),
+        width = footer.attr('width'),
+        height = footer.attr('height');
+    footer.removeAttr('width');
+    footer.removeAttr('height');
+    footer.get(0).setAttribute('viewBox', '0 0 ' + width + ' ' + height);
+    footer.find('.twinkle').removeAttr('style');
+  });
+  //$('#side').load('/images/home-side.svg', function() {
+    //var side = $('#side svg'),
+        //width = side.attr('width'),
+        //height = side.attr('height');
+    //side.removeAttr('width');
+    //side.removeAttr('height');
+    //side.get(0).setAttribute('viewBox', '0 0 ' + width + ' ' + (+height));
+    ////side.get(0).setAttribute('preserveAspectRatio', 'xMaxYMax');
+    //side.find('.twinkle').removeAttr('style');
+  //});
 });
